38eb9f38250a08acec716fee87a7da14576b6870
Gaurav Pardeshi <ompardeshi16@gmail.com>
2024-10-15 17:24:41 +0530

    add db

--- demo/settings.py
+++ demo/settings.py
@@ -67,11 +67,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres-db-shopmeet',
+        'USER': 'Shopmeet',
+        'PASSWORD': 'Password@123',
+        'HOST': 'postgres-db-shoopmeet.postgres.database.azure.com',
+        'PORT': 3306,
+    }}
 
 if ENVIRONMENT == 'production':
     DEBUG = True
